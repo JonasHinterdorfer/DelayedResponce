f3d84aa2a027021b63037530f47c0bc796f07e48
admin <jonas.hinterdorfer@ionas999.at>
2024-05-06 08:53:39 +0200

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,62 @@
+namespace Test;
+
+public class Program
+{
+    public static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(0, int.MaxValue);
+
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = summaries[Random.Shared.Next(summaries.Length)]
+                        })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+        app.MapGet("/wait", async () =>
+        {
+            await Program.semaphoreSlim.WaitAsync();
+            return Results.Ok("Proceed");
+        });
+        app.MapGet("/start", () =>
+        {
+            Program.semaphoreSlim.Release(int.MaxValue);
+            return Results.Ok("Started");
+        });
+        app.Run();
+    }
+}
